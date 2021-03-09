13d88198b4692ac61998760ccb1e66b7538dbaeb
geoffroyp2 <g.poyferre@gmail.com>
2021-03-09 17:18:12 +0100

    ovenEngine file structure

--- ovenEngine/src/engine/engine.ts
+++ ovenEngine/src/engine/engine.ts
@@ -1,132 +1,150 @@
-import { WSINClient } from "../com/wsCom/WSINClient";
-import { WSOUTClient } from "../com/wsCom/WSOUTClient";
+import emul from "../comEmulation/emul";
+import { GQLMutation, GQLQuery, GQLSubscribe } from "../network/GQLClient";
+import { getTargetQuery, getUpdateSensorsQuery, liveSubscriptionQuery } from "../utils/queries";
+import WSINClient from "../network/WSINClient";
+import WSOUTClient from "../network/WSOUTClient";
+import { LiveStatusType, SensorValuesType, Target } from "../types/APITypes";
+import { isLiveSubscribeData, isTargetQueryRes } from "../utils/typeCheck";
 
-import { Reference } from "../sharedTypes/dbModelTypes";
-import { emptyValues, EngineState, EngineStatusType, ValuesType } from "./types";
+class Engine {
+  // status from api
+  private status: LiveStatusType = "stop";
+  private target: Target = {};
 
-import { ReferenceHandler } from "./referenceHandler";
-import graphMemory from "./graphMemory";
+  // connection instances
+  private wsin: WSINClient;
+  private wsout: WSOUTClient;
 
-export class Engine {
-  private WSIN: WSINClient;
-  private WSOUT: WSOUTClient;
-  private refH: ReferenceHandler | null;
-
-  public status: EngineStatusType = "stop";
-  public values: ValuesType = emptyValues;
-
-  private loopInterval: number = 2000; // main loop interval in ms
-  private programStartTime: number = 0; // absolute start date
-  private programPauseTime: number = 0; // total pause time
-  private programRunTime: number = 0; // total run time
-  private lastPause: number = 0; // absolute date for last pause
+  // internal program variables
+  private programStartTime: number = 0;
+  private programRunTime: number = 0;
+  private programPauseTime: number = 0;
+  private lastPause: number = 0;
+  private targetValues: SensorValuesType = {
+    oxygen: 0,
+    temperature: 0,
+  };
+  private sensorValues: SensorValuesType = {
+    oxygen: 0,
+    temperature: 0,
+  };
 
   constructor() {
-    this.WSIN = new WSINClient((values) => {
-      // console.log(values);
-      this.values.sensor = values;
-    });
-    this.WSIN.connect();
-
-    this.WSOUT = new WSOUTClient();
-    this.WSOUT.connect();
-
-    graphMemory.reset();
+    GQLSubscribe(liveSubscriptionQuery, (data: any) => this.handleLiveUpdate(data));
+    this.wsin = new WSINClient((data: any) => this.updateSensors(data));
+    this.wsout = new WSOUTClient();
   }
 
-  public loadRef(ref: Reference) {
-    this.refH = new ReferenceHandler(ref);
+  private async handleLiveUpdate(data: any) {
+    if (isLiveSubscribeData(data)) {
+      const newState = data.data.live;
+
+      switch (newState.status) {
+        case "start":
+          this.start();
+          break;
+        case "pause":
+          this.pause();
+          break;
+        case "stop":
+          this.stop();
+          break;
+      }
+
+      if (newState.currentTargetId !== this.target.id) {
+        this.loadTarget(newState.currentTargetId);
+      }
+    }
   }
 
-  public getRef() {
-    return this.refH?.ref ?? "default";
+  public updateSensors(values: any) {
+    console.log(values);
   }
 
-  public ping(): boolean {
-    return this.WSIN.getStatus() && this.WSOUT.getStatus();
+  public async loadTarget(id: number) {
+    const targetQueryRes = await GQLQuery(getTargetQuery(id));
+    if (isTargetQueryRes(targetQueryRes.data)) {
+      const results = targetQueryRes.data.targets.rows;
+      if (results[0]) {
+        this.target = results[0];
+        console.log(`Loaded new target ${this.target.name} (id: ${this.target.id})`);
+      } else {
+        console.error("could not load target");
+      }
+    }
   }
 
-  public getState(): EngineState {
-    if (this.refH) this.values.target.temperature = this.refH.targetTemp;
-    return {
-      values: this.values,
-      status: this.status,
-      refID: this.refH?.ref._id ?? "default",
-      times: {
-        start: this.programStartTime,
-        totalRun: this.programRunTime,
-        totalPause: this.programPauseTime,
-      },
-    };
+  public run() {
+    if (this.status === "start") {
+      const now = new Date().getTime();
+      this.programRunTime = now - this.programStartTime + this.programPauseTime; // TODO : mode not to count pause time
+
+      // 1. calculate target temperature and send it to wsout
+      this.refreshTargetValues();
+      emul.sendTarget(this.targetValues);
+      // this.wsout.sendTemperature(this.targetValues.temperature);
+
+      // 2. send current sensor values from wsin to API
+      this.sensorValues = emul.getSensorValues(this.programRunTime);
+      GQLMutation(getUpdateSensorsQuery(this.sensorValues));
+    }
   }
 
-  public getGraphs() {
-    return graphMemory.getGraphs();
-  }
-
-  public start() {
-    if (this.refH && this.status === "stop") {
-      graphMemory.reset();
+  private start() {
+    if (this.status === "stop") {
+      this.status = "start";
+      this.programStartTime = new Date().getTime();
       this.programRunTime = 0;
       this.programPauseTime = 0;
-      this.programStartTime = new Date().getTime();
-      this.status = "run";
-
-      console.log("Program Started");
-
-      this.run();
-    }
-  }
-
-  public stop() {
-    if (this.status === "run" || this.status === "pause") {
-      this.status = "stop";
-
-      console.log("Program Stopped");
-    }
-  }
-
-  public pause() {
-    if (this.status === "run") {
-      this.lastPause = new Date().getTime();
-      this.status = "pause";
-
-      console.log("Program Paused");
-    }
-  }
-
-  public unpause() {
-    if (this.status === "pause") {
-      // add pause time
+      console.log("Program started");
+    } else if (this.status === "pause") {
+      // TODO: unpause
+      this.status = "start";
       this.programPauseTime += new Date().getTime() - this.lastPause;
-      this.status = "run";
+      console.log("Program unpaused");
+    }
+  }
 
-      console.log("Program Unpaused");
+  private pause() {
+    if (this.status === "start") {
+      // TODO: pause
+      this.status = "pause";
+      this.lastPause = new Date().getTime();
+      console.log("Program paused");
+    }
+  }
 
-      this.run();
+  private stop() {
+    if (this.status === "start" || this.status === "pause") {
+      // TODO: stop
+      this.status = "stop";
+      console.log("Program stopped");
     }
   }
 
   public reconnect() {
     console.log("trying to reconnect...");
-
-    this.WSIN.connect();
-    this.WSOUT.connect();
+    this.wsin.connect();
+    this.wsout.connect();
   }
 
-  private run() {
-    if (this.status === "run") {
-      setTimeout(() => {
-        const now = new Date().getTime();
-        this.programRunTime = now - this.programStartTime + this.programPauseTime; // TODO : mode not to count pause time
+  private refreshTargetValues() {
+    if (this.target.points) {
+      const points = this.target.points;
+      const time = Math.floor(this.programRunTime / 1000); // in seconds
 
-        this.refH.refreshTargetTemp(this.programRunTime);
-        this.WSOUT.sendTemperature(this.refH.targetTemp);
-        graphMemory.addValue(this.programRunTime, this.refH.targetTemp);
-        if (this.status === "run") this.run(); // another check if paused/stopped in-between
-      }, this.loopInterval);
+      // find index for current time
+      let idx = 1;
+      for (; idx < points.length; idx++) {
+        if (points[idx].time > time) break;
+      }
+
+      // calculate interpolation
+      const pente = (points[idx].temperature - points[idx - 1].temperature) / (points[idx].time - points[idx - 1].time);
+      const yOrigine = points[idx].temperature - pente * points[idx].time;
+      this.targetValues.temperature = time * pente + yOrigine;
     }
   }
 }
 
-export default new Engine();
+export default Engine;
