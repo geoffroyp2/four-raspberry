964940ba92c39441e5401b35fc948c16e42338fe
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-06-25 19:21:01 +0200

    Added Back button

--- frontend/src/app/editor/graphs/records/RecordInfosPage.tsx
+++ frontend/src/app/editor/graphs/records/RecordInfosPage.tsx
@@ -1,23 +1,33 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
 import RecordGraph from "./RecordGraph";
 import RecordInfos from "./RecordInfos";
 import RecordGallery from "./RecordGallery";
+
 import MainGrid, { MainGridItem } from "@components/grids/MainGrid";
+import BackButton from "@components/buttons/BackButton";
 
 const RecordInfosPage: FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <MainGrid cols="1" xlRows="home-xl-1" xlCols="home-xl-3/2">
-      <MainGridItem col="1" row="1" xlCol="1" xlRow="1">
-        <RecordGraph />
-      </MainGridItem>
-      <MainGridItem col="1" row="2" xlCol="2" xlRow="1">
-        <RecordInfos />
-      </MainGridItem>
-      <MainGridItem col="1" row="3" xlCol="1" xlRow="2">
-        <RecordGallery />
-      </MainGridItem>
-    </MainGrid>
+    <>
+      <MainGrid cols="1" xlRows="home-xl-1" xlCols="home-xl-3/2">
+        <MainGridItem col="1" row="1" xlCol="1" xlRow="1">
+          <BackButton onClick={() => navigate("../../")} />
+        </MainGridItem>
+        <MainGridItem col="1" row="2" xlCol="1" xlRow="2">
+          <RecordGraph />
+        </MainGridItem>
+        <MainGridItem col="1" row="3" xlCol="2" xlRow="2">
+          <RecordInfos />
+        </MainGridItem>
+        <MainGridItem col="1" row="4" xlCol="1" xlRow="3">
+          <RecordGallery />
+        </MainGridItem>
+      </MainGrid>
+    </>
   );
 };
 
--- frontend/src/app/editor/graphs/targets/TargetInfosPage.tsx
+++ frontend/src/app/editor/graphs/targets/TargetInfosPage.tsx
@@ -1,20 +1,28 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
 // import TargetGraph from "./TargetGraph";
 import TargetInfos from "./TargetInfos";
 // import TargetGallery from "./TargetGallery";
+
 import MainGrid, { MainGridItem } from "@components/grids/MainGrid";
+import BackButton from "@components/buttons/BackButton";
 
 const TargetInfosPage: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <MainGrid cols="1" xlRows="home-xl-1" xlCols="home-xl-3/2">
       <MainGridItem col="1" row="1" xlCol="1" xlRow="1">
+        <BackButton onClick={() => navigate("../../")} />
+      </MainGridItem>
+      <MainGridItem col="1" row="2" xlCol="1" xlRow="2">
         {/* <TargetGraph /> */}
       </MainGridItem>
-      <MainGridItem col="1" row="2" xlCol="2" xlRow="1">
+      <MainGridItem col="1" row="3" xlCol="2" xlRow="2">
         <TargetInfos />
       </MainGridItem>
-      <MainGridItem col="1" row="3" xlCol="1" xlRow="2">
+      <MainGridItem col="1" row="4" xlCol="1" xlRow="3">
         {/* <TargetGallery /> */}
       </MainGridItem>
     </MainGrid>
