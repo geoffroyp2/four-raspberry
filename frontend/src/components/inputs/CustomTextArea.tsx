75dcf7c74bf7f565cc488d0d7ea27e6e633aaf14
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-06-26 11:37:38 +0200

    TextArea auto resize

--- frontend/src/components/inputs/CustomTextArea.tsx
+++ frontend/src/components/inputs/CustomTextArea.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 
 type Props = {
   value: string;
@@ -6,13 +6,23 @@ type Props = {
 };
 
 const CustomTextArea: FC<Props> = ({ value, onChange }) => {
+  const textRef = useRef<(Element & HTMLTextAreaElement) | null>(null);
+  const [Height, setHeight] = useState<string>("auto");
+
   return (
     <textarea
+      ref={textRef}
       autoFocus
       onFocus={({ currentTarget }) => currentTarget.select()}
-      className="text-gray-900 w-full px-3 rounded-md  focus:shadow-outline"
+      className="text-gray-900 w-full px-3 rounded-md focus:shadow-outline leading-tight"
       value={value}
+      onInput={() => setHeight((textRef.current?.scrollHeight ?? 20) + "px")}
       onChange={({ target }) => onChange(target.value)}
+      style={{
+        resize: "none",
+        height: Height,
+        maxHeight: "140px",
+      }}
     />
   );
 };
