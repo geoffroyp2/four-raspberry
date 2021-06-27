143825a671a8dd0753a12094bc017630d3ace514
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-06-27 20:38:56 +0200

    Added selection border for gallery

--- frontend/src/components/cards/GalleryCard.tsx
+++ frontend/src/components/cards/GalleryCard.tsx
@@ -13,6 +13,7 @@ const GalleryCard: FC<Props> = ({ images, onImgClick }) => {
   const [ShowModal, setShowModal] = useState<boolean>(false);
   const [ModalUrl, setModalUrl] = useState<string>("");
   const [Page, setPage] = useState<number>(0);
+  const [Selected, setSelected] = useState<number | null>(null);
 
   const getImgPage = () => {
     const imgPage = [];
@@ -28,11 +29,16 @@ const GalleryCard: FC<Props> = ({ images, onImgClick }) => {
         <div className="grid grid-cols-4 gap-2 p-3">
           {getImgPage().map((e, i) => (
             <img
-              className="rounded-md shadow-md w-max cursor-pointer border-4 border-transparent hover:border-red-500"
+              className={`rounded-md shadow-md w-max cursor-pointer border-4 ${
+                Selected === i ? "border-purple-500" : "border-transparent"
+              } hover:border-red-500`}
               key={`img-${i}`}
               src={e.url}
               alt=""
-              onClick={() => onImgClick && onImgClick(e.id)}
+              onClick={() => {
+                onImgClick && onImgClick(e.id);
+                setSelected(i);
+              }}
               onDoubleClick={() => {
                 setModalUrl(e.url);
                 setShowModal(true);
@@ -40,7 +46,15 @@ const GalleryCard: FC<Props> = ({ images, onImgClick }) => {
             />
           ))}
         </div>
-        <Pagination small pageAmount={Math.floor(images.length / 12)} currentPage={Page} handleSetPage={setPage} />
+        <Pagination
+          small
+          pageAmount={Math.floor(images.length / 12)}
+          currentPage={Page}
+          handleSetPage={(page) => {
+            setPage(page);
+            setSelected(null);
+          }}
+        />
       </BasicMainCard>
       <ImageModal show={ShowModal} hide={() => setShowModal(false)} url={ModalUrl} />
     </>
