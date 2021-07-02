a2a5f24d5682f71f3ee2f9e5cfefcdd8ba0e9594
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-07-02 12:59:07 +0200

    Added slight color gradient to cards & background

--- frontend/src/components/cards/BasicMainCard.tsx
+++ frontend/src/components/cards/BasicMainCard.tsx
@@ -3,7 +3,14 @@ import { FC } from "react";
 const BasicMainCard: FC = ({ children }) => {
   return (
     <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-      <div className="inline-block min-w-full shadow rounded-lg overflow-hidden bg-gray-700 border-gray-900">{children}</div>
+      <div
+        className="inline-block min-w-full shadow rounded-lg overflow-hidden border-gray-900"
+        style={{
+          background: "linear-gradient(180deg, #3f3f46 0%, #36363D 100%)",
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 };
