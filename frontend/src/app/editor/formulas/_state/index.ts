c07f8dd38ea87b73788868d4c7642eff49e54da6
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-07-02 18:04:48 +0200

    Added formula store

---
+++ frontend/src/app/editor/formulas/_state/index.ts
@@ -0,0 +1,9 @@
+import formulaData from "./formulaDataSlice";
+import formulaDisplay from "./formulaDisplaySlice";
+
+const formulaReducers = {
+  formulaData,
+  formulaDisplay,
+};
+
+export default formulaReducers;
--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,12 +1,14 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import appState from "./_state/appStateSlice";
-import graphReducers from "./editor/graphs/_state";
-import pieceReducers from "./editor/pieces/_state";
+import graphReducers from "@editor/graphs/_state";
+import pieceReducers from "@editor/pieces/_state";
+import formulaReducers from "@editor/formulas/_state";
 
 export const store = configureStore({
   reducer: {
     ...graphReducers,
     ...pieceReducers,
+    ...formulaReducers,
     appState,
   },
 });
