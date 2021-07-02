5b5dd489fe5f85ae92e3b47221498e88b3b1744d
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-07-02 19:21:02 +0200

    Updated Packages

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { selectAppTheme } from "./_state/appStateSlice";
 
 import AppRouter from "./AppRouter";
-import "./App.scss";
+import "./App.css";
 
 const App: FC = () => {
   const theme = useSelector(selectAppTheme);
