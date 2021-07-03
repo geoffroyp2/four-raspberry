bfcb70565de5dd3daefb7c363a0d9130deb5fbec
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-07-03 22:56:13 +0200

    changed apollo config to point to the raspberry

--- frontend/src/apolloClient/config.ts
+++ frontend/src/apolloClient/config.ts
@@ -1,16 +1,16 @@
 import { WebSocketLink } from "@apollo/client/link/ws";
 
 export const HttpConfig = {
-  uri: "http://localhost:3001/graphql",
+  uri: "http://192.168.0.121:3001/graphql",
 };
 
 export const WsConfig: WebSocketLink.Configuration = {
-  uri: "ws://localhost:3001/graphql",
+  uri: "ws://192.168.0.121:3001/graphql",
   options: {
     reconnect: true,
   },
 };
 
 export const uploadConfig = {
-  uri: "http://localhost:3001/graphql",
+  uri: "http://192.168.0.121:3001/graphql",
 };
