4bd5b96a3e73142522c1c763dec8f6c6b67f28d2
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2021-07-05 16:56:13 +0200

    clean up

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -3,12 +3,14 @@ import appState from "./_state/appStateSlice";
 import graphReducers from "@editor/graphs/_state";
 import pieceReducers from "@editor/pieces/_state";
 import formulaReducers from "@editor/formulas/_state";
+import liveScreenReducer from "./live/_state/liveScreenSlice";
 
 export const store = configureStore({
   reducer: {
     ...graphReducers,
     ...pieceReducers,
     ...formulaReducers,
+    live: liveScreenReducer,
     appState,
   },
 });
